df3d82209a9092f11c38cf8daaece4f4ab583309
gentry-atkinson <gma23@txstate.edu>
2024-07-11 14:11:27 -0500

    working on purchase dataset

---
+++ make_purchase_data.py
@@ -0,0 +1,5 @@
+# Author: Gentry Atkinson (gatkins0@stedwards.edu)
+# Organization: St. Edwards Universtiy
+# Date: 11 July, 2024
+
+MANUFACTURERS = ['CompuCraft', 'R&L', 'ValuServe', 'TSN', 'Soft Systems', 'Cherry']
