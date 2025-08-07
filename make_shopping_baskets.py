082c3a419dffa1487b3c7efec1020c24abc8352a
Gentry Atkinson <gatkinso@stedwards.edu>
2025-08-07 14:29:25 -0500

    added more products and a frequent 3-set

--- make_shopping_baskets.py
+++ make_shopping_baskets.py
@@ -6,7 +6,7 @@ import random
 import pandas as pd
 
 NUM_STRONG_PAIRS = 5
-NUM_WEAK_PAIRS = 5
+NUM_WEAK_PAIRS = 7
 NUM_ROWS = 50_000
 
 WITH_NOISE = False
@@ -17,23 +17,39 @@ all_products = [
     "eggs", "beef", "ham", "butter", "milk", "yogurt", "sugar", "flour", "honey",
     "yeast", "cocoa", "corn", "bacon", "shrimp", "salsa", "jelly", "beans",
     "tuna", "vinegar", "soap", "sponges", "batteries", "detergent", "mayo", 
-    "toothpaste", "coffee", "mustard", "paprika", "cinamon", "limes", "peaches",
+    "toothpaste", "coffee", "mustard", "paprika", "cinnamon", "limes", "peaches",
     "celery", "onions", "feta", "cookies", "buns", "pita", "salmon", "bleach",
-    "foil", "lotion", "mouthwash", "sunscreen", "diapers"
+    "foil", "lotion", "mouthwash", "sunscreen", "diapers", "granola", 
+    "canned soup", "bottled water", "lettuce", "green tea", "grapes", "ketchup",
+    "baby food", "pears", "mangoes", "oatmeal", "dish soap", "noodles", "grits",
+    "potato chips", "peanut butter", "marmalade"
 ]
+
+for p in all_products:
+    assert all_products.count(p) == 1
 
 all_shoppers = random.sample(range(100_000, 999_999), 500)
 all_stores = list(range(1, 51))
 
-strong_pairs = [(a, b) for a, b in [random.sample(all_products, 2) for _ in range(NUM_STRONG_PAIRS)]]
+strong_triple = random.sample(all_products, 3)
+strong_pairs = [
+    (strong_triple[0], strong_triple[1]), 
+    (strong_triple[0], strong_triple[2]), 
+    (strong_triple[1], strong_triple[2]) 
+]
+strong_pairs = strong_pairs + [(a, b) for a, b in [random.sample(all_products, 2) for _ in range(NUM_STRONG_PAIRS-3)]]
 weak_pairs = [(a, b) for a, b in [random.sample(all_products, 2) for _ in range(NUM_STRONG_PAIRS)]]
+
 favorite_product = random.choice(all_products)
+while favorite_product in strong_triple or any([favorite_product in t for t in strong_pairs]):
+    favorite_product = random.choice(all_products)
 
 if __name__ == '__main__':
     shoppers = []
     stores = []
     baskets = []
 
+    print("Strong Triple: ", strong_triple)
     print("Strong Pairs: ", strong_pairs)
     print("Weak Pairs: ", weak_pairs)
     print(f"Number of possible products: {len(all_products)}")
@@ -42,14 +58,28 @@ if __name__ == '__main__':
     for _ in range(NUM_ROWS):
         shoppers.append(random.choice(all_shoppers))
         stores.append(random.choice(all_stores))
-        basket_size = random.randint(5, 15)
+        basket_size = random.randint(3, 15)
         basket = set(random.sample(all_products, basket_size))
         # TREND- strong pairs appear together frequently
+        if random.randint(0, 19) == 19:
+            basket.add(strong_triple[0])
+        elif random.randint(0, 19) == 19:
+            basket.add(strong_triple[1])
+        elif random.randint(0, 19) == 19:
+            basket.add(strong_triple[1])
+
         for a, b in strong_pairs:
             if a in basket and random.randint(0,1)==1:
                 basket.add(b)
             elif b in basket and random.randint(0,1)==1:
-                basket.add(a) 
+                basket.add(a)
+
+        if strong_triple[0] in basket and strong_triple[1] in basket and random.randint(0,1)==1:
+            basket.add(strong_triple[2])
+        elif strong_triple[0] in basket and strong_triple[2] in basket and random.randint(0,1)==1:
+            basket.add(strong_triple[1])
+        elif strong_triple[1] in basket and strong_triple[2] in basket and random.randint(0,1)==1:
+            basket.add(strong_triple[0])
 
         # TREND- weak pairs appear less frequently but more than random
         for a, b in weak_pairs:
